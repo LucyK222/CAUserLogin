9d7e0bbe3869b610b4ab374f32b1d6d0c3fbc99e
jiayunkang <jaren.kang@mail.utoronto.ca>
2024-10-23 18:46:23 -0400

    phase1_task2.1_jaren.kang

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Return???????????????.
+     * @param name the name?????
+     */
+    void setCurrentUser(String name);
 }
